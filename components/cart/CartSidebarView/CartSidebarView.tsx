dcd313ab5f915ecd52873ef944a0a2bd121e6bc1
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-03-30 22:29:31 +0800

    Update CartSidebarView.tsx

--- components/cart/CartSidebarView/CartSidebarView.tsx
+++ components/cart/CartSidebarView/CartSidebarView.tsx
@@ -119,8 +119,8 @@ const CartSidebarView: FC = () => {
                   <span>Calculated at checkout</span>
                 </li>
                 <li className="flex justify-between py-1">
-                  <span>Estimated Shipping</span>
-                  <span className="font-bold tracking-wide">FREE</span>
+                  <span>Shipping</span>
+                  <span>Calculated at checkout</span>
                 </li>
               </ul>
               <div className="flex justify-between border-t border-accents-3 py-3 font-bold mb-10">
